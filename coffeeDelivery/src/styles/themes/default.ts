e03b79a53d2478cc3a0492d282741146edfc3148
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-08-16 19:46:07 -0300

    style: definido style guide

---
+++ coffeeDelivery/src/styles/themes/default.ts
@@ -0,0 +1,42 @@
+export const defaultThemes = {
+
+  color: {
+    'yellow-light': '#F1E9C9',
+    'yellow': '#DBAC2C',
+    'yellow-dark': '#C47F17',
+
+    'purple-light': '#EBE5F9',
+    'purple': '#8047F8',
+    'purple-dark': '#4B2995',
+
+    'white': '#FFFFFF',
+    'background': '#FAFAFA',
+    'base-card': '#F3F2F2',
+    'base-input': '#EDEDED',
+    'base-button': '#E6E5E5',
+    'base-hover': '#D7D5D5',
+    'base-label': '#8D8686',
+    'base-text': '#574F4D',
+    'base-subtitle':'#403937',
+    'base-title': '#272221',
+  },
+
+  font_roboto: {
+    'Text L': '1.25rem',
+    'Text M': '1rem',
+    'Text S': '0.875rem',
+    'Text XS': '0.75rem',
+    'Tag': '0.625rem',
+    'Button G': '0.875rem',
+    'Button M': '0.75rem',
+  },
+
+  font_baloo: {
+    'Title XL': '3rem',
+    'Title L': '2rem',
+    'Title M': '1.5rem',
+    'Title S': '1.25rem',
+    'Title XS': '1.125rem',
+  }
+
+}
