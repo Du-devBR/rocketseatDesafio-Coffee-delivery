72be9bffb938b244230d0b476446c3bd33f26574
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-08-31 22:00:14 -0300

    fix: corrigido bug deploy

--- coffeeDelivery/src/pages/Checkout/index.tsx
+++ coffeeDelivery/src/pages/Checkout/index.tsx
@@ -37,7 +37,8 @@ export function Checkout(){
     },
   });
 
-  // const {user} = orderState
+  const {} = orderState
+  // console.log(user)
 
   const [dataPayment, setDataPayment] = useState<IDataPayment>({
     id: '',
